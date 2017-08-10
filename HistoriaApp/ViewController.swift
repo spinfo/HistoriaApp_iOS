711ff5898753a32889df4e930ac2eb270c2c66f8
David <david@schnickschnack.spinfo.uni-koeln.de>
2017-08-10 11:13:16 +0200

    Initial Commit

---
+++ HistoriaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HistoriaApp
+//
+//  Created by David on 10.08.17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
